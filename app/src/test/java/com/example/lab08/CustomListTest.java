eed5767b744c47e80c59f03231ccdc63b503b774
Sean Mah <seankmah@gmail.com>
2025-10-28 10:11:39 -0600

    test: Added testHasCity

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -3,5 +3,13 @@ package com.example.lab08;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+    @Test
+    public void testHasCity() {
+        CustomList customList = new CustomList();
+        City city = new City("Edmonton", "Alberta");
 
+        customList.addCity(city);
+
+        assertTrue(customList.hasCity(city));
+    }
 }
