62e5a0afd127b18975fd55d7ae151e85cd0b53a1
Sean Mah <seankmah@gmail.com>
2025-10-28 10:19:49 -0600

    test: Created testDeleteCity() test case

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -1,15 +1,35 @@
 package com.example.lab08;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
+    private CustomList customList;
+
+    @BeforeEach
+    public void setup() {
+        customList = new CustomList();
+    }
+
     @Test
     public void testHasCity() {
-        CustomList customList = new CustomList();
-        City city = new City("Edmonton", "Alberta");
+        City city = getMockCity();
 
         customList.addCity(city);
 
         assertTrue(customList.hasCity(city));
+    }
+
+    @Test
+    public void testDeleteCity() {
+        City city = getMockCity();
+
+        customList.deleteCity(city);
+
+        assertFalse(customList.hasCity(city));
+    }
+
+    private City getMockCity() {
+        return new City("Edmonton", "Alberta");
     }
 }
